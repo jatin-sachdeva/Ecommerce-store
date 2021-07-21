8c68dfade51b44343ddab7cb8e8034fe1a44c9ec
JatinS <jatin@JatinS>
2021-07-21 17:09:38 +0530

    building the signup form- form data parsing

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+
 const app = express(); // start the express server
+
+// middleware func provided by express to parse body
+// body-parser has been depreciated
+app.use(express.urlencoded({ extended: true }));
 
 // a function to respond to the get request to '/' ie the home page
 app.get('/', (req, res) => {
@@ -13,28 +18,28 @@ app.get('/', (req, res) => {
 		</form>`
 	);
 });
+// // using a  custom middleware function
+// const parseData = (req, res, next) => {
+// 	req.on('data', (data) => {
+// 		// data will be a buffer and has to be converted to string
+// 		// all buffer data should be converted to string
+// 		const rawData = data.toString();
+// 		const dataArr = rawData.split('&');
+// 		const formData = {};
+// 		dataArr.forEach((curr) => {
+// 			const [ key, value ] = curr.split('=');
+// 			formData[key] = value;
+// 		});
+// 		req.body = formData;
+// 		next();
+// 	});
+// };
+
 // handelling the post request sent by thr form on submission
 app.post('/', (req, res) => {
-	//  looking for a data event on the req
-	req.on('data', (data) => {
-		// data will be a buffer and has to be converted to string
-		// all buffer data should be converted to string
-		const rawData = data.toString();
-
-		const formData = parseData(rawData);
-		console.log(formData);
-	});
+	console.log(req.body);
 });
-const parseData = (rawData) => {
-	const dataArr = rawData.split('&');
-	const formData = {};
-	dataArr.forEach((curr) => {
-		const [ key, value ] = curr.split('=');
-		formData[key] = value;
-	});
-	return formData;
-};
-// lspecify the port no to listen the traffic from;
+
 app.listen(3000, () => {
 	console.log('listening to port 3000 on localhost');
 });
